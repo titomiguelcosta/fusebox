cd70a506d05a8845783c287080651a057c17937d
Tito Miguel Costa <tito@pixelfusion.co.nz>
2017-11-07 15:44:57 +1300

    bug fixe for cloudwatch settings

--- fusebox/fusebox/settings.py
+++ fusebox/fusebox/settings.py
@@ -107,33 +107,34 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-boto3_session = Session(
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID', ''),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY', ''),
-    region_name=os.getenv('AWS_REGION', 'ap-southeast-2')
-)
+if os.getenv('AWS_ACCESS_KEY_ID', False) and os.getenv('AWS_SECRET_ACCESS_KEY', False):
+    boto3_session = Session(
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID', ''),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY', ''),
+        region_name=os.getenv('AWS_REGION', 'ap-southeast-2')
+    )
 
-LOGGING = {
-    'version': 1,
-    'handlers': {
-        'watchtower':  {
-            'level': 'DEBUG',
-            'class': 'watchtower.CloudWatchLogHandler',
-            'log_group': 'fusebox',
-            'boto3_session': boto3_session
+    LOGGING = {
+        'version': 1,
+        'handlers': {
+            'watchtower':  {
+                'level': 'DEBUG',
+                'class': 'watchtower.CloudWatchLogHandler',
+                'log_group': 'fusebox',
+                'boto3_session': boto3_session
+            },
+            'console': {
+                'class': 'logging.StreamHandler',
+            },
         },
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': [os.getenv("LOGGER_HANDLER", "console")],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
+        'loggers': {
+            'django': {
+                'handlers': ['watchtower'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        }
     }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
