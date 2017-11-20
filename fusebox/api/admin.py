1da65d0d0eb4c8560ae7515245ac77356718e38f
Tito Miguel Costa <tito@pixelfusion.co.nz>
2017-11-21 11:39:07 +1300

    added models to admin area

--- fusebox/api/admin.py
+++ fusebox/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Artist, Track, Played, Rate
+
+admin.site.register(Artist)
+admin.site.register(Played)
+admin.site.register(Track)
+admin.site.register(Rate)
