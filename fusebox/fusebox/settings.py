c31bd4b9db0256aae078762e1cf8a451cffd4004
Tito Miguel Costa <tito@pixelfusion.co.nz>
2017-11-22 15:46:30 +1300

    allow all to connect to server

--- fusebox/fusebox/settings.py
+++ fusebox/fusebox/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'wrqfp6%v80-*bi$w!c9#a(*gb#o*_#dok)ej%=n*1=p#-vq_3i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool("True" == os.getenv("DEBUG", "False"))
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.amazonaws.com', '.ngrok.io', '.pxlfsn.co']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.amazonaws.com', '.ngrok.io', '.pxlfsn.co', '*']
 
 # Application definition
 
