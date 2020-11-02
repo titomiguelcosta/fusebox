c2edbdc8cb410d860034562716b9684ca46de102
Tito Miguel Costa <titomiguelcosta@gmail.com>
2020-11-02 23:04:06 +0100

    fixed bug on track class

--- web/src/Track.js
+++ web/src/Track.js
@@ -15,29 +15,31 @@ class Track extends React.Component {
     }
 
     render() {
-        <tr>
-            <td>{this.props.artists}</td>
-            <td>{this.props.title}</td>
-            <td>{this.props.album}</td>
-            <td>
-                <div className="dropdown">
-                    <button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Rate</button>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 0)}>0</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 1)}>1</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 2)}>2</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 3)}>3</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 4)}>4</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 5)}>5</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 6)}>6</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 7)}>7</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 8)}>8</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 9)}>9</a>
-                        <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 10)}>10</a>
+        return (
+            <tr>
+                <td>{this.props.artists}</td>
+                <td>{this.props.title}</td>
+                <td>{this.props.album}</td>
+                <td>
+                    <div className="dropdown">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Rate</button>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 0)}>0</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 1)}>1</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 2)}>2</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 3)}>3</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 4)}>4</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 5)}>5</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 6)}>6</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 7)}>7</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 8)}>8</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 9)}>9</a>
+                            <a className="dropdown-item" href="#" onClick={(e) => this.handleRate(e, 10)}>10</a>
+                        </div>
                     </div>
-                </div>
-            </td>
-        </tr>
+                </td>
+            </tr>
+        );
     }
 }
 
